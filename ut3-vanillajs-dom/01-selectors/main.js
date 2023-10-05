45bc171f0ab81e421cbaeffabc9d094f3779ef18
= <=>
2023-10-05 10:39:20 +0200

    Ejercicios 1 de javascript DOM

--- ut3-vanillajs-dom/01-selectors/main.js
+++ ut3-vanillajs-dom/01-selectors/main.js
@@ -8,13 +8,13 @@ const PARRAFO1 = document.querySelectorAll(".parrafo");
 
 
 console.log(TITULO.innerHTML + "-> get via getElementById");
-console.log(PARRAFO[0].innerHTML + "-> get via getElementsByClassName")
-console.log(PARRAFO[1].innerHTML + "-> get via getElementsByClassName")
-console.log(NOMBRE[0].name + "-> get via getElementsByName")
-console.log(APELLIDO[0].name + "-> get via getElementsByName")
-console.log(ELEMENTO[0].innerHTML + "-> get via getElementsByTagName")
-console.log(ELEMENTO[1].innerHTML + "-> get via getElementsByTagName")
-console.log(ELEMENTO[2].innerHTML + "-> get via getElementsByTagName")
-console.log(TITULO1.innerHTML + "-> get via querySelector")
-console.log(PARRAFO1[0].innerHTML + "-> get via querySelectorAll")
-console.log(PARRAFO1[1].innerHTML + "-> get via querySelectorAll")
+console.log(PARRAFO[0].innerHTML + "-> get via getElementsByClassName");
+console.log(PARRAFO[1].innerHTML + "-> get via getElementsByClassName");
+console.log(NOMBRE[0].name + "-> get via getElementsByName");
+console.log(APELLIDO[0].name + "-> get via getElementsByName");
+console.log(ELEMENTO[0].innerHTML + "-> get via getElementsByTagName");
+console.log(ELEMENTO[1].innerHTML + "-> get via getElementsByTagName");
+console.log(ELEMENTO[2].innerHTML + "-> get via getElementsByTagName");
+console.log(TITULO1.innerHTML + "-> get via querySelector");
+console.log(PARRAFO1[0].innerHTML + "-> get via querySelectorAll");
+console.log(PARRAFO1[1].innerHTML + "-> get via querySelectorAll");
