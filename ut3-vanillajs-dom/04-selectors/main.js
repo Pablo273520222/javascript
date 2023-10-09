f95ecb8fb1d12b260dcb2dc7ff114fdd0409abfb
= <=>
2023-10-09 09:51:04 +0200

    Ejercicios 4 de javascript del tema 3

---
+++ ut3-vanillajs-dom/04-selectors/main.js
@@ -0,0 +1,37 @@
+const BODY = document.body;
+BODY.classList.add('p-3', 'm-0', 'border-0', 'bd-example');
+const LISTGROUPITEM = document.getElementsByClassName('list-group');
+
+const ARRAYSTEXT = [
+  'A simple default list group item',
+  'A simple primary list group item',
+  'A simple secondary list group item',
+  'A simple success list group item',
+  'A simple danger list group item',
+  'A simple warning list group item',
+  'A simple info list group item',
+  'A simple light list group item',
+  'A simple dark list group item'
+];
+
+const ARRAYSCLASS = [
+  [],
+  ['list-group-item-primary'],
+  ['list-group-item-secondary'],
+  ['list-group-item-success'],
+  ['list-group-item-danger'],
+  ['list-group-item-warning'],
+  ['list-group-item-info'],
+  ['list-group-item-light'],
+  ['list-group-item-dark']
+];
+
+for (const LISTGROUP of LISTGROUPITEM) {
+  for (let i = 0; i < ARRAYSTEXT.length; i++) {
+    const LIST = document.createElement('a');
+    LIST.href = "#";
+    LIST.textContent = ARRAYSTEXT[i];
+    LIST.classList.add('list-group-item', 'list-group-item-action', ...ARRAYSCLASS[i]);
+    LISTGROUP.appendChild(LIST);
+  }
+}
